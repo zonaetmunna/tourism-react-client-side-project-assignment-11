5ab56c6acad58ff0521810d1c7c8292f289e2c89
zonaetmunna <zonaetmonna@gmail.com>
2021-10-29 23:33:32 +0600

    madd

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-     apiKey: "AIzaSyCliEtHs13YYvhGW47HVHgq8rjPMtZOPWg",
-     authDomain: "travel-react-node.firebaseapp.com",
-     projectId: "travel-react-node",
-     storageBucket: "travel-react-node.appspot.com",
-     messagingSenderId: "74342844361",
-     appId: "1:74342844361:web:5bdba872c6d2cb38078a18"
+     apiKey: "AIzaSyBnAVK-IjlnK4qQi44XpdYqnhwvGAPsR00",
+     authDomain: "react-router-firebase-2e6e9.firebaseapp.com",
+     projectId: "react-router-firebase-2e6e9",
+     storageBucket: "react-router-firebase-2e6e9.appspot.com",
+     messagingSenderId: "1039431700295",
+     appId: "1:1039431700295:web:22fca149fe8a66ac774662"
 };
 export default firebaseConfig;
