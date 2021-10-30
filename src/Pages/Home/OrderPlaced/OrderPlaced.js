7739639045518500333ab6926b16a0544a19774d
zonaetmunna <zonaetmonna@gmail.com>
2021-10-30 11:42:16 +0600

    dd

--- src/Pages/Home/OrderPlaced/OrderPlaced.js
+++ src/Pages/Home/OrderPlaced/OrderPlaced.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Card, Col, Container, Row, Button } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+
 import { useParams } from 'react-router';
 
 const OrderPlaced = () => {
      const { id } = useParams();
      const [service, setService] = useState([]);
+
      useEffect(() => {
           const url = `https://calm-tor-36170.herokuapp.com/services/${id}`;
           fetch(url)
@@ -19,31 +22,36 @@ const OrderPlaced = () => {
 
      const handleAddBooking = (id) => {
 
+     }
 
-          const url = 'https://calm-tor-36170.herokuapp.com/services';
+     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+     const onSubmit = (data) => {
+          const url = 'http://localhost:5000/orderService';
           fetch(url, {
                method: "POST",
                headers: {
                     'content-type': 'application/json'
                },
-               body: JSON.stringify(id)
+               body: JSON.stringify(data)
           })
                .then(res => res.json())
                .then(data => {
+                    console.log(data)
                     if (data.insertedId) {
 
                          alert('order added successfully');
 
                     }
                })
-     }
+
+     };
 
      return (
           <div>
                <h1>{id}</h1>
                <Container>
-                    <Row>
-                         <Col sm={8}>
+                    <Row className="">
+                         <Col sm={6}>
                               <Card >
                                    <Card.Img variant="top" src={service.img} />
                                    <Card.Body>
@@ -53,7 +61,25 @@ const OrderPlaced = () => {
                                    </Card.Body>
                               </Card>
                          </Col>
+                         <Col>
+                              <h3>Please Give your information</h3>
+                              <form onSubmit={handleSubmit(onSubmit)}>
+                                   <input {...register("name")} placeholder="your name" className="mb-2" />
+                                   <br />
+                                   <input {...register("email")} placeholder="your email" className="mb-2" />
+                                   <br />
+                                   <input {...register("id")} placeholder="service id" className="mb-2" />
+                                   <br />
+                                   <input {...register("service name")} placeholder="service name" className="mb-2" />
+                                   <br />
+                                   <input {...register("address")} placeholder="your address" className="mb-2" />
+                                   <br />
+
+                                   <input type="submit" value="booking" />
+                              </form>
+                         </Col>
                     </Row>
+
                </Container>
           </div>
      );
--- src/Pages/Myorder/MyOrder.js
+++ src/Pages/Myorder/MyOrder.js
@@ -1,17 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
 const MyOrder = () => {
-     const [order, setOrder] = useState([]);
+     const [orders, setOrders] = useState([]);
      useEffect(() => {
-          fetch('https://calm-tor-36170.herokuapp.com/services')
+          fetch('http://localhost:5000//orderService')
                .then(res => res.json())
                .then(data => {
-                    setOrder(data);
+                    setOrders(data);
                })
      }, [])
      return (
           <div>
-               <h1>order{order}</h1>
+               <Row xs={1} md={2} className="g-4">
+                    {
+                         orders.map(order => <Col>
+                              <Card>
+                                   <Card.Img variant="top" src="holder.js/100px160" />
+                                   <Card.Body>
+                                        <Card.Title>{order.service_name}</Card.Title>
+                                        <Card.Text>
+                                             {order.email}
+                                        </Card.Text>
+                                   </Card.Body>
+                              </Card>
+                         </Col>)
+                    }
+               </Row>
           </div>
      );
 };
